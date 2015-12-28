5e0b6af0441706ccf3ac4c1b0a47521bdfca618a
sdaniels <daniels.scott.r@gmail.com>
2015-12-28 10:06:12 -0500

    Updated country api to retrieve by id, region and query on name

--- app/routes/api.js
+++ app/routes/api.js
@@ -8,14 +8,55 @@ module.exports = function (app, atlasDb) {
 };
 
 router.get('/api/:type', function (req, res, next) {
-
-  var apiType = req.params.type;
+  var apiType = req.params.type ? req.params.type.toLowerCase() : "";
+  var id = req.query.id;
+  var region = req.query.region;
+  var query = req.query.query;
+  var queryValue = {};
+  var json = { resp: [] };
   res.setHeader('Content-Type', 'application/json');
 
-  Country.find(function (err, countries) {
-    console.log(countries);
-  });
+  switch (apiType) {
+    case "country":
+      if (id !== undefined || region !== undefined) {
+        if (id !== undefined) {
+          queryValue.id = id;
+        } else {
+          queryValue.region = region;
+        }
+
+        Country.find(queryValue, function (err, countries) {
+          if (!err) {
+            json.resp = countries;
+          } else {
+            json.error = err;
+          }
+          res.send(JSON.stringify(json, null, 3));
+        });
+      } else if (query !== undefined) {
+        Country.find({ name: new RegExp('^' + query, "i") }, function (err, countries) {
+          if (!err) {
+            json.resp = countries;
+          } else {
+            json.error = err;
+          }
+          res.send(JSON.stringify(json, null, 3));
+        });
+      } else {
+        Country.find(function (err, countries) {
+          if (!err) {
+            json.resp = countries;
+          } else {
+            json.error = err;
+          }
+          res.send(JSON.stringify(json, null, 3));
+        });
+      }
+      break;
+    default:
+      json.error = "No API type found!";
+      res.json(JSON.stringify(json, null, 3))
+  }
 
 
-  res.send(JSON.stringify({ a: 1 }, null, 3));
 });
