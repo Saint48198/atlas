3838f71ba40204f5156ef534101d14715c0e38d9
sdaniels <daniels.scott.r@gmail.com>
2016-01-23 08:22:56 -0500

    created simple wikipedia api

---
+++ app/routes/api/wikipedia.js
@@ -0,0 +1,27 @@
+"use strict";
+
+var express = require('express');
+var request = require('request');
+var router = express.Router();
+
+
+module.exports = function (app) {
+  app.use('/', router);
+};
+
+router.get('/api/wikipedia', (req, res, next) => {
+  res.setHeader('Content-Type', 'application/json');
+
+  let url = 'https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=Main%20Page';
+
+  request(url, (error, response, body) => {
+    let data = { resp: { error: error } };
+
+    if (!error && response.statusCode == 200) {
+      data.resp = JSON.parse(body);
+    }
+
+    res.send(JSON.stringify(data, null, 3));
+  });
+
+});
