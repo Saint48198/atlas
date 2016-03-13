f5ce2a03b4b24a36b04fb606bec6ce3da351d133
sdaniels <daniels.scott.r@gmail.com>
2016-03-13 17:28:55 -0400

    updated for the page title to get updated when the route changes

--- public/ts/app.component.ts
+++ public/ts/app.component.ts
@@ -1,18 +1,19 @@
-import {Component}                      from 'angular2/core';
-import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
-import {AboutComponent}                 from './about/about.component';
-import {HomeComponent}                  from './home/home.component';
-import {RegionComponent}                from './region/region.component';
-import {CountryComponent}               from './country/country.component';
-import {RegionService}                  from './common/region.service';
-import {CountryService}                 from './common/country.service';
-import {WikipediaService}               from './common/wikipedia.service';
+import {Component}                              from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES, Router} from 'angular2/router';
+import {Title}                                  from 'angular2/platform/browser';
+import {AboutComponent}                         from './about/about.component';
+import {HomeComponent}                          from './home/home.component';
+import {RegionComponent}                        from './region/region.component';
+import {CountryComponent}                       from './country/country.component';
+import {RegionService}                          from './common/region.service';
+import {CountryService}                         from './common/country.service';
+import {WikipediaService}                       from './common/wikipedia.service';
 
 @Component({
   selector: 'app',
   templateUrl: '../../templates/app.component.html',
   directives: [ ROUTER_DIRECTIVES ],
-  providers: [RegionService, CountryService, WikipediaService]
+  providers: [RegionService, CountryService, WikipediaService, Title]
 })
 @RouteConfig([
   { path: '/',            name: 'Home',        component: HomeComponent,        useAsDefault: true  },
@@ -20,4 +21,19 @@ import {WikipediaService}               from './common/wikipedia.service';
   { path: '/region/:id',  name: 'Region',      component: RegionComponent                           },
   { path: '/country/:id', name: 'Country',     component: CountryComponent                          }
 ])
-export class AppComponent {}
+export class AppComponent {
+  constructor(router: Router, title: Title) {
+    router.subscribe((url) => {
+      const pageTitle = this.returnPageTitleFromUrl(url);
+
+      title.setTitle("Atlas" + pageTitle);
+    });
+  }
+
+  returnPageTitleFromUrl(url: String) {
+    const urlData = url.split("/");
+    const title = ": " + urlData[0].charAt(0).toUpperCase() + urlData[0].slice(1);
+
+    return title;
+  }
+}
